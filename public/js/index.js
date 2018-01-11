a49c8aeb6767015087415a40a92d109ad4455e9d
Han Chiang <yaphc@hotmail.com>
2018-01-11 20:17:06 +0800

    Emit new message on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ let socket = io();
 
 socket.on('connect', function() {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'jen@example.com',
-    text: 'Hey there'
-  });
 });
 
 socket.on('newMessage', function(message) {
