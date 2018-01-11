9d0031d3c4a5f450ffbfb0bad8f7b904b56c9bcc
Han Chiang <yaphc@hotmail.com>
2018-01-11 20:30:40 +0800

    Greet new user and alert others

--- public/js/index.js
+++ public/js/index.js
@@ -8,6 +8,10 @@ socket.on('newMessage', function(message) {
   console.log('Got new message:', message);
 });
 
+socket.on('newUserJoined', function(message) {
+  console.log('From: ' + message.from + ', text: ' + message.text);
+});
+
 socket.on('disconnect', function() {
   console.log('Disconnected from server');
 });
